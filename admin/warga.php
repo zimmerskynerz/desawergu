7c48b1abe6036ba9de71a49f7245d35788208e01
zimmerskynerz <99anakrantau99@gmail.com>
2020-12-21 08:11:44 +0700

    wergu

--- admin/warga.php
+++ admin/warga.php
@@ -54,7 +54,7 @@ $row = mysqli_fetch_array($result);
 				<div class="container-fluid">
 					<div class="row mb-2">
 						<div class="col-sm-6">
-							<h1 class="m-0 text-dark">DATA WARGA DESA KAYEN</h1>
+							<h1 class="m-0 text-dark">DATA WARGA DESA WERGU WETAN</h1>
 						</div><!-- /.col -->
 					</div><!-- /.row -->
 				</div><!-- /.container-fluid -->
